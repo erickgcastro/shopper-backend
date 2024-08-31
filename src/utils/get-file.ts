7006f3e7259f8fce37118709c7bc19c317eea780
Erick Gabriel <limaerick.03@gmail.com>
2024-08-31 19:02:09 -0300

    fix: image route

--- src/utils/get-file.ts
+++ src/utils/get-file.ts
@@ -1,6 +1,10 @@
 import path from "node:path"
 
 export const getFile = (file: string) => {
-  const fileDir = path.resolve(__dirname, "..", "uploads", file)
+  const fileDir = path.resolve(
+    __dirname,
+    `${process.env.NODE_ENV === "DEV" ? "../" : ""}uploads`,
+    file
+  )
   return fileDir
 }
