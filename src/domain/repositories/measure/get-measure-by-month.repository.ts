879bd86aeecb70fb2f45db6d51c30ed85b78e88b
Erick Gabriel <limaerick.03@gmail.com>
2024-08-31 21:51:06 -0300

    chore: func doc

--- src/domain/repositories/measure/get-measure-by-month.repository.ts
+++ src/domain/repositories/measure/get-measure-by-month.repository.ts
@@ -1,6 +1,11 @@
 import { Measure } from "@prisma/client"
 
 export interface IGetMeasureByMonthRepository {
+  /**
+   * This function retrieves measures from a database for a specific month.
+   * @param {number} month - The month number to fetch records for (0 for January, 1 for February, etc.).
+   * Note: JavaScript months are zero-based, so January is 0 and December is 11.
+   */
   getByMounth: (mounth: number) => IGetMeasureByMonthRepository.Result
 }
 
--- src/infra/respositories/measure.repository.ts
+++ src/infra/respositories/measure.repository.ts
@@ -60,11 +60,6 @@ export class MeasureRepository
     })
   }
 
-  /**
-   * This TypeScript function retrieves measures from a database for a specific month.
-   * @param {number} month - The month number to fetch records for (0 for January, 1 for February, etc.).
-   * Note: JavaScript months are zero-based, so January is 0 and December is 11.
-   */
   async getByMounth(month: number): IGetMeasureByMonthRepository.Result {
     const currentYear = new Date().getFullYear()
     const startOfMonth = new Date(currentYear, month, 1)
